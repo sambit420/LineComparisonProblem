b9f7d923a33ebd8de82705c1bfb2bf4d702ecbb8
sambit420 <sambit2024@gmail.com>
2022-10-20 18:49:01 +0530

    Welcome to Line Comparison Computation Program

---
+++ src/com/line/comparison/LineComparisonComputation.java
@@ -0,0 +1,7 @@
+package com.line.comparison;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
