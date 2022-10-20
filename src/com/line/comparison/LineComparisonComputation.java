825cbb41b864be249c255988cdf39253f9a20d27
sambit420 <sambit2024@gmail.com>
2022-10-20 19:12:07 +0530

    Added : Use Case 2 - Checking the equality of two lines

--- src/com/line/comparison/LineComparisonComputation.java
+++ src/com/line/comparison/LineComparisonComputation.java
@@ -13,8 +13,21 @@ public class LineComparisonComputation {
         //Initialize the object.
         LineComparisonComputation line = new LineComparisonComputation();
 
-        //Declaring the variables.
         Scanner scan = new Scanner(System.in);
+        String s1 = "Line 1";
+        String s2 = "Line 2";
+        String l1 = Double.toString(setLines(scan,s1));
+        System.out.println();
+        String l2 = Double.toString(setLines(scan,s2));
+        checkEquality(l1,l2);
+
+    }
+
+    //Setting values for lines.
+    public static double setLines(Scanner scan, String s) {
+        System.out.println("Enter "+s+" End Points:-");
+
+        //Declaring the variables.
         System.out.print("Enter First Co-ordinates(x1,y1): \n");
         double x1= scan.nextDouble();
         double y1= scan.nextDouble();
@@ -22,18 +35,25 @@ public class LineComparisonComputation {
         System.out.print("Enter Second Co-ordinates(x2,y2): \n");
         double x2= scan.nextDouble();
         double y2= scan.nextDouble();
-        scan.close();
         System.out.printf("Second Co-ordinates (x2= %g , y2= %g)%n",x2,y2);
-        getLength(x1,x2,y1,y2);
-
+        double length = getLength(x1,x2,y1,y2,s);
+        return length;
     }
 
-    public static void getLength(double x1, double x2, double y1, double y2) {
+    //Getting length of the line.
+    public static double getLength(double x1, double x2, double y1, double y2, String s) {
         double a = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
         double length = (double)Math.sqrt(a);
         System.out.println();
-        System.out.println("Length of a line: "+length);
+        System.out.println("Length of "+s+" is : "+length);
+        return length;
     }
 
+    //Checking if both are equals or not
+    public static void checkEquality(String l1, String l2) {
+        boolean equal = l1.equals(l2);
+        System.out.println("Equality of two lines using Java equals method is : "+equal);
     }
+
+}
 
